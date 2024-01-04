7a1492ada4fb2786e35985236884b27ed0e9eded
Bern N <bernnystrom@gmail.com>
2024-01-04 22:38:52 +0800

    🎉 (new): Initial commit and remove storyboards

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Bern N on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
