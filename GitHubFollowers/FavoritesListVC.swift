c6e792b9392619c075cc9c6e573d18b27bcb281a
Bern N <bernnystrom@gmail.com>
2024-01-05 00:10:27 +0800

    ✨ (feat): Add tab bar with search and favorites nav controllers

--- GitHubFollowers/FavoritesListVC.swift
+++ GitHubFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GitHubFollowers
 //
 //  Created by Bern N on 1/4/24.
@@ -7,14 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemTeal
+
+        view.backgroundColor = .systemBlue
     }
 
-
 }
-
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Bern N on 1/4/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+    }
+
+}
