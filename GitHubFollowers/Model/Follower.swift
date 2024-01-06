5393996aeda94bd47cdaac6ea30ec73cec73aa61
Bern N <bernnystrom@gmail.com>
2024-01-06 10:14:04 +0800

    🧐 (feat): Add follower and user model to store retrieved data from api

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Bern N on 1/6/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
