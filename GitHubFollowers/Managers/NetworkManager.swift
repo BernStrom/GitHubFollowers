fd71afb3c89b62b142f727e8c6dfb9c2beb10429
Bern N <bernnystrom@gmail.com>
2024-01-09 12:52:27 +0800

    ✨ (feat): Add method for downloading and caching avatar image
    This method will check for any cached avatar images on device and set them on display on screen, before making any network call to download the images. A network call to download the avatar image will only be made if the image doesn't exist in cache.

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Bern N on 1/7/24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    private let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
@@ -46,6 +47,45 @@ class NetworkManager {
                 completed(.success(followers))
             } catch {
                 completed(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func downloadImage(from urlString: String, completed: @escaping (UIImage) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if error != nil {
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            
+            DispatchQueue.main.async {
+                completed(image)
             }
         }
         
