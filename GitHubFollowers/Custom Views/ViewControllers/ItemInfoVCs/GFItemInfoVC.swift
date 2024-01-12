0ddafce599094b8cde30382ae0b782e84f469438
Bern N <bernnystrom@gmail.com>
2024-01-12 15:20:48 +0800

    🚚 (style): Move item info view controller file into its own folder

--- GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
+++ GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
@@ -14,8 +14,19 @@ class GFItemInfoVC: UIViewController {
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
     
+    var user: User!
+    
     let padding: CGFloat = 20
-
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
