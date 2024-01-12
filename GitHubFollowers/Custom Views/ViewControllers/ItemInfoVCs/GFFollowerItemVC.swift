ca96c722c4f886ed15d3263eaf7db4a3e98c9a63
Bern N <bernnystrom@gmail.com>
2024-01-12 15:32:45 +0800

    💄 (style): Add custom view controllers for user repos and followers
    These are subclasses to the GFItemInfoVC, which contains the stack views with the UI components for the user info screen.

---
+++ GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GitHubFollowers
+//
+//  Created by Bern N on 1/12/24.
+//
+
+import Foundation
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+    
+}
