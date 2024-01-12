719e97db90247f5edb5561fde681ebd2f0af9113
Bern N <bernnystrom@gmail.com>
2024-01-12 13:02:45 +0800

    🎨 (style): Add a constants file with reusable properties
    This also improves the experience while developing by reducing errors in hard-coded values, such as texts and numbers values.

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Bern N on 1/12/24.
+//
+
+import Foundation
+
+struct SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
