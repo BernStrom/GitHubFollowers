929a844d33abf882595f605ff697b370b25f141d
Bern N <bernnystrom@gmail.com>
2024-01-16 14:14:04 +0800

    ♻️ (refactor): Change parent class of UIViewController to custom GFDataLoadingVC

--- GitHubFollowers/Screens/FollowersListVC.swift
+++ GitHubFollowers/Screens/FollowersListVC.swift
@@ -11,7 +11,7 @@ protocol FollowerListVCDelegate: AnyObject {
     func didRequestFollower(for username: String)
 }
 
-class FollowersListVC: UIViewController {
+class FollowersListVC: GFDataLoadingVC {
     
     enum Section {
         case main
