a162c1c2db187e15fbae2a1f45705e20257d6a7a
Bern N <bernnystrom@gmail.com>
2024-01-18 17:30:40 +0800

    🐛 (fix): Display empty state view after removing all users from favorites screen

--- GitHubFollowers/Custom Views/Views/GFEmptyStateView.swift
+++ GitHubFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -47,13 +47,13 @@ class GFEmptyStateView: UIView {
         logoImageView.image = Images.emptyStateLogo
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         
-        let logoBottomConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 80 : 150
+        let logoBottomConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 80 : 40
         
         NSLayoutConstraint.activate([
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
-            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
-            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: logoBottomConstant)
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
+            logoImageView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: logoBottomConstant)
         ])
     }
     
